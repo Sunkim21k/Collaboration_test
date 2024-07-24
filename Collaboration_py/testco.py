77142a59d7dd8505a2e78717c2299dd7c21d8ddb
sunkimgithub <sunkimgithub@gmail.com>
2024-07-24 15:15:45 +0900

    add develop branch

--- Collaboration_py/testco.py
+++ Collaboration_py/testco.py
@@ -11,3 +11,9 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+import sys
+num1, num2 = map(int, sys.stdin.readline().split())
+print(add(num1, num2))
+print(sub(num1, num2))
+print(multiply(num1, num2))
+print(divide(num1, num2))
