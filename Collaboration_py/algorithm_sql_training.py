f6e41ed929d7e2864767ade9dc20d6b222d58336
Sunkim <sunhyuk22k@gmail.com>
2024-09-30 02:07:12 +0900

    algorithm training update

--- Collaboration_py/algorithm_sql_training.py
+++ Collaboration_py/algorithm_sql_training.py
@@ -410,3 +410,18 @@
 # BJ 15894 정사각형
 # n = int(input())
 # print(n*4)
+
+# BJ 9063 대지
+# import sys
+# n = int(input())
+# numbers_x = []
+# numbers_y = []
+# for i in range(n):
+#     a, b = map(int, sys.stdin.readline().split())
+#     numbers_x.append(a)
+#     numbers_y.append(b)
+#
+# if min(numbers_x) == max(numbers_x) or min(numbers_y) == max(numbers_y):
+#     print(0)
+# else:
+#     print((max(numbers_x) - min(numbers_x)) * (max(numbers_y) - min(numbers_y)))
